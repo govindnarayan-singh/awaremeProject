d06625a244f405e1b459977500cb2b8882462426
heygovindnarayan <70362547+heygovindnarayan@users.noreply.github.com>
2021-04-15 09:29:37 +0530

    Update views.py

--- awareme/awaremeapp/views.py
+++ awareme/awaremeapp/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 from .models import *
 from .forms import *
 from django.http import HttpResponse
